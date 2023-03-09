a334969c9e5450b04373e2d8904504aa1d932953
Rina <pamelkk@yandex.ru>
2023-03-09 21:39:47 +0300

    добавила роутер

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../../pages/HomePage/HomePage';
+import Login from '../../pages/Login/Login';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+import Room from '../../pages/Room/Room';
 
 type AppProps = {
   countForRent: number;
 }
 
-function App({countForRent}: AppProps): JSX.Element {
+function App({ countForRent }: AppProps): JSX.Element {
   return (
-    <HomePage countForRent={countForRent} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<HomePage countForRent={countForRent} />} />
+        <Route path='/offer/:id' element={<Room />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
