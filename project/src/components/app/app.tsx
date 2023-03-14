46da5a63968129822b22049cc0af03aa4b1d96e6
Rina <pamelkk@yandex.ru>
2023-03-14 19:34:01 +0300

    добавила разметку формы, карточек, моки

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -3,17 +3,19 @@ import HomePage from '../../pages/HomePage/HomePage';
 import Login from '../../pages/Login/Login';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 import Room from '../../pages/Room/Room';
+import { offersType } from '../../types/types';
 
 type AppProps = {
   countForRent: number;
+  offers: offersType[];
 }
 
-function App({ countForRent }: AppProps): JSX.Element {
+function App({ countForRent, offers }: AppProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
-        <Route path='/' element={<HomePage countForRent={countForRent} />} />
+        <Route path='/' element={<HomePage countForRent={countForRent} offers={offers} />} />
         <Route path='/offer/:id' element={<Room />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
--- project/src/pages/HomePage/HomePage.tsx
+++ project/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
-import Card from './Card/Card';
+import { offersType } from '../../types/types';
+import CardsList from './CardsList/CardsList';
 
 type HomePageProps = {
   countForRent: number;
+  offers: offersType[];
 }
 
-const HomePage = ({countForRent}: HomePageProps) => (
+const HomePage = ({countForRent, offers}: HomePageProps): JSX.Element => (
   <div className="page page--gray page--main">
     <div style={{display: 'none'}}>
       <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path></symbol></svg>
@@ -95,13 +97,7 @@ const HomePage = ({countForRent}: HomePageProps) => (
                 <li className="places__option" tabIndex={0}>Top rated first</li>
               </ul>
             </form>
-            <div className="cities__places-list places__list tabs__content">
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-            </div>
+            <CardsList offers={offers} />
           </section>
           <div className="cities__right-section">
             <section className="cities__map map"></section>
